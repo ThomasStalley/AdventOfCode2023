7a3858d17b0cad5282447638529d753fdd29e814
ThomasStalley <tstalley@realla.co>
2023-12-01 12:21:07 +0000

    DAY01

--- AdventOfCode2023/day_01.py
+++ AdventOfCode2023/day_01.py
@@ -1,42 +1,45 @@
 import re
 
 with open("AdventOfCode2023/InputData/input_01.txt", "r") as file:
-    calibrations = [row.strip() for row in file.readlines()]
+    unclean_calibrations = [row.strip() for row in file.readlines()]
+
+# Mappings to replace string numbers with digits:
+word_to_digit_crossover_mapping = {
+    "oneight": "18",
+    "threeight": "38",
+    "fiveight": "58",
+    "nineight": "98",
+    "sevenine": "79",
+    "twone": "21",
+    "eighthree": "83",
+    "eightwo": "82"
+}
+word_to_digit_singles_mapping = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+word_to_digit_mapping = word_to_digit_crossover_mapping | word_to_digit_singles_mapping
 
 
 def part_one(input_list):
+    """Q: What is the sum of all of the calibration values?"""
     total_calibration_p1 = 0
     for calibration in input_list:
         digits_only_string = "".join(re.findall(r"\d", calibration))
-        partial_calibration = digits_only_string[0] + digits_only_string[-1]
-        total_calibration_p1 += int(partial_calibration)
+        partial_calibration_string = digits_only_string[0] + digits_only_string[-1]
+        total_calibration_p1 += int(partial_calibration_string)
     return total_calibration_p1
 
 
 def part_two(input_list):
-    word_to_digit_crossover_mapping = {
-        "oneight": "18",
-        "threeight": "38",
-        "fiveight": "58",
-        "nineight": "98",
-        "sevenine": "79",
-        "twone": "21",
-        "eighthree": "83",
-        "eightwo": "82"
-    }
-    word_to_digit_singles_mapping = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-
-    word_to_digit_mapping = word_to_digit_crossover_mapping | word_to_digit_singles_mapping
+    """Q: What is the sum of all of the calibration values?"""
     total_calibration_p2 = 0
     for calibration in input_list:
         for key in word_to_digit_mapping:
@@ -47,5 +50,5 @@ def part_two(input_list):
     return total_calibration_p2
 
 
-print("-> Part One:", part_one(calibrations))
-print("-> Part Two:", part_two(calibrations))
+print("-> Part One:", part_one(unclean_calibrations))
+print("-> Part Two:", part_two(unclean_calibrations))
--- AdventOfCode2023/test_day01.py
+++ AdventOfCode2023/test_day01.py
@@ -1,15 +1,14 @@
 from AdventOfCode2023 import day_01
 
-with open("AdventOfCode2023/SampleData/sample_01_1.txt", "r") as file:
-    sample_data_one = [row.strip() for row in file.readlines()]
-
-with open("AdventOfCode2023/SampleData/sample_01_2.txt", "r") as file:
-    sample_data_two = [row.strip() for row in file.readlines()]
+sample_data_one = [row.strip() for row in open("AdventOfCode2023/SampleData/sample_01_1.txt", "r").readlines()]
+sample_answer_one = 142
+sample_data_two = [row.strip() for row in open("AdventOfCode2023/SampleData/sample_01_2.txt", "r").readlines()]
+sample_answer_two = 281
 
 
 def test_part_one():
-    assert day_01.part_one(sample_data_one) == 142
+    assert day_01.part_one(sample_data_one) == sample_answer_one
 
 
 def test_part_two():
-    assert day_01.part_two(sample_data_two) == 281
+    assert day_01.part_two(sample_data_two) == sample_answer_two
